418aabca7d8d9e5441cf490137e96337e22da0b2
leo <Leopwn@hotmail.com>
2023-09-10 13:34:46 +0200

    Added functions: RemoveStaff & StaffList
    Added new entry to the menu: "stafflist"
    
    Added functions:
        RemoveStaff: Removes staff or trainee from staffList/traineeList
        StaffList: Shows list of all current members of both staffList and traineeList
    
    NOTE: has preset staff and trainees for easier debugging

--- Main.java
+++ Main.java
@@ -5,13 +5,21 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static ArrayList staffList = new ArrayList();
-    public static ArrayList traineeList = new ArrayList<>();
+    public static ArrayList<Staff> staffList = new ArrayList<>();
+    public static ArrayList<Trainee> traineeList = new ArrayList<>();
 
     public static void main(String[] args) {
 
-        menu();
+        // Create preset staff
+        LocalDate currentDate = LocalDate.now();
+        staffList.add(new Staff(100, "Leeroy Jenkins", "Human Paladin", 30000, currentDate));
+        staffList.add(new Staff(101, "Hanz Fritz", "khajiit", 20500, currentDate));
+        staffList.add(new Staff(102, "Luigi", "Game character", 15000, currentDate));
 
+        traineeList.add(new Trainee(103, "Link", "Game Character", LocalDate.of(2024, 9, 8), "Great swordsmanship"));
+        traineeList.add(new Trainee(104, "Leon", "Zombie apocalypse survivor", LocalDate.of(2025, 5, 3), "Great zombie slayer"));
+
+        menu();
     }
 
     public static void menu() {
@@ -22,21 +30,29 @@ public class Main {
             System.out.println("'add': add a staff or trainee to the system." +
                     "\n'remove': remove staff or trainee from the system." +
                     "\n'edit': edit a staff or trainee previously added to the system." +
+                    "\n'stafflist: Show all staff and trainees" +
                     "\n'exit': Quit the program");
             System.out.println("\u001B[0m");
+
             switch (scanner.nextLine()) {
                 case "add" -> {
                     RegisterStaff();
                     break;
                 }
                 case "remove" -> {
+                    RemoveStaff();
                     break;
                 }
                 case "edit" -> {
                     break;
                 }
+                case "stafflist" -> {
+                    StaffList();
+                    break;
+                }
                 case "exit" -> System.exit(0);
             }
+
         } while (!menuBool);
     }
 
@@ -107,5 +123,86 @@ public class Main {
                                 "\nCredentials: " + trainee.getCredentials());
             }
         }
+    }
+
+    public static void RemoveStaff() {
+        System.out.println(
+                "\nStaff:");
+        for (Staff obj : staffList) {
+            System.out.println(
+                    "Name: " + obj.getName() + " ID: " + obj.getId() + " Gender: " + obj.getGender()
+            );
+        }
+        System.out.println(
+                "\nTrainee:");
+        for (Trainee obj : traineeList) {
+            System.out.println(
+                    "Name: " + obj.getName() + " ID: " + obj.getId() + " Gender: " + obj.getGender()
+            );
+        }
+        Scanner scanner = new Scanner(System.in);
+        String searchValue = scanner.nextLine();
+        boolean found = false;
+        Object foundItem = "null";
+
+        for (Staff item : staffList) {
+            if (item.getId() == Integer.parseInt(searchValue)) {
+                found = true;
+                foundItem = "ID: " + item.getId() + "\nNAME: " + item.getName();
+                break;
+            }
+        }
+
+        for (Trainee item : traineeList) {
+            if (item.getId() == Integer.parseInt(searchValue)) {
+                found = true;
+                foundItem = "ID: " + item.getId() + "\nNAME: " + item.getName();
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.print(foundItem + "\n\u001B[31mRemove?\u001B[0m");
+            System.out.print(" (y/n): ");
+            switch (scanner.nextLine()) {
+                case "y", "Y":
+                    staffList.removeIf(staff -> staff.getId() == (Integer.parseInt(searchValue)));
+
+                case "n", "N":
+                    break;
+            }
+
+        } else {
+            System.out.println("Search result: None found!");
+
+            System.out.print("\npress ENTER to return...");
+            scanner.nextLine();
+        }
+    }
+
+    public static void StaffList() {
+
+        System.out.println(
+                "\nStaff:");
+        for (Staff obj : staffList) { // List all staff
+            System.out.println(
+                    "Name: " + obj.getName() + " ID: " + obj.getId() + " Gender: " + obj.getGender()
+            );
+        }
+        System.out.println(
+                "\nTrainee:");
+        for (Trainee obj : traineeList) { // List all trainees
+            System.out.println(
+                    "Name: " + obj.getName() + " ID: " + obj.getId() + " Gender: " + obj.getGender()
+            );
+        }
+
+        System.out.println("Total: " + (staffList.size() + traineeList.size()) +
+                " \nStaff: " + staffList.size() +
+                " \nTrainees: " + traineeList.size());
+
+        System.out.print("\npress ENTER to return...");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
     }
 }
